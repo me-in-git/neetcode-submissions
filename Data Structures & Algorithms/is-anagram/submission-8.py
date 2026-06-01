4a0519b6fac5b7b7e8050ac6c22d88e8c55d1e9b
me-in-git <pragnyatv@gmail.com>
2026-06-01 14:22:09 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # strarr1=[0]*26
+        # strarr2=[0]*26
+        # if len(s)!=len(t):
+        #     return False
+        # for i in range(len(s)):
+        #     strarr1[ord(s[i])-97]+=1
+        #     strarr2[ord(t[i])-97]+=1
+        
+        # if strarr1==strarr2:
+        #     return True
+        # else:
+        #     return False
+
+        strarr=[0]*26
+        if len(s)!=len(t):
+            return False
+        for i in range(len(s)):
+            strarr[ord(s[i])-97]+=1
+            strarr[ord(t[i])-97]-=1
+
+        if (strarr== [0]*26):
+            return True
+        else:
+            return False
+
+            
