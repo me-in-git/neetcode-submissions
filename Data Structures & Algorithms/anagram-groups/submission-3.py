5986a69b35468ffb60c445add2ee024b8df08999
me-in-git <pragnyatv@gmail.com>
2026-06-01 14:52:02 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def getlist(string):
+            lis=[0]*26
+            for ch in string:
+                lis[ord(ch)-97]+=1
+            return tuple(lis)
+
+        
+        hashmap=defaultdict(list)
+        for word in strs:
+            hashmap[getlist(word)].append(word)
+
+        return list(hashmap.values())
+
+        ## Can be done by sorting the strings instead too
+        
+
+
+
